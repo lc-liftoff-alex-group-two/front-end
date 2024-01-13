39087c338fdbd79838c51b4c2b6e8b88c94f985f
Sam <ssmarie14@yahoo.com>
2024-01-13 16:11:56 -0600

    updated changes

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from 'react-bootstrap/Button'  
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1> Hello, this is my first React App</h1>
+      <Button>This is Button</Button>
+      
+        <h1> I am learning React</h1>
+        
+    </div>
+    
+  );
+  
+}
+
+export default App;
