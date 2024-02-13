a540ee5d15ef785d76df2425d8cca5dfd2a09b04
lavliy <natarajanlavanya92@gmail.com>
2024-02-13 16:08:21 -0600

    productpage

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,22 +1,44 @@
-import React from 'react';
+
 import ProductCard from './ProductCard';  
+import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+//import Products from './Products';
 
 
 const ProductPage = () => {
-  const products = [
-    // product data here
-  ];
+  // const [products, setProducts] = useState();
+  // const { id } = useParams();
 
-  return (
-    <div>
-      <h2>Product Page</h2>
-      <div className="product-container">
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
-    </div>
-  );
+  // useEffect(() =>{
+  //   const fetchProduct = async () => {
+  //     try{
+  //       const response = await fetch (`http://localhost:8080/products/${id}`);
+  //       const data = await response.json();
+  //       setProducts(data);
+  //     } catch(error){
+  //       console.error('Error fetching product:', error);
+  //     }
+  //   };
+  //   fetchProduct();
+  // }, [id]);
+
+  // if (!products) {
+  //   return <div>Loading...</div>;
+  // }
+
+ 
+  // return (
+  //   <div>
+  //     <h2>Product Page</h2>
+  //     <div className="product-container">
+  //     {/* <ProductCard key={product.id} product={product} /> */}
+  //       {products.map((product) => (
+  //         <ProductCard key={product.id} product={product} />
+  //       ))}
+  //     </div>
+      
+  //   </div>
+ // );
 };
 
 export default ProductPage;
