ac914d0db881d6417fe3bb7309419b85037fec80
lavliy <natarajanlavanya92@gmail.com>
2024-02-14 14:37:44 -0600

    user changes

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -6,39 +6,39 @@ import React, { useState, useEffect } from 'react';
 
 
 const ProductPage = () => {
-  // const [products, setProducts] = useState();
-  // const { id } = useParams();
+  const [products, setProducts] = useState();
+  const { id } = useParams();
 
-  // useEffect(() =>{
-  //   const fetchProduct = async () => {
-  //     try{
-  //       const response = await fetch (`http://localhost:8080/products/${id}`);
-  //       const data = await response.json();
-  //       setProducts(data);
-  //     } catch(error){
-  //       console.error('Error fetching product:', error);
-  //     }
-  //   };
-  //   fetchProduct();
-  // }, [id]);
+  useEffect(() =>{
+    const fetchProduct = async () => {
+      try{
+        const response = await fetch (`http://localhost:8080/products/${id}`);
+        const data = await response.json();
+        setProducts(data);
+      } catch(error){
+        console.error('Error fetching product:', error);
+      }
+    };
+    fetchProduct();
+  }, [id]);
 
-  // if (!products) {
-  //   return <div>Loading...</div>;
-  // }
+  if (!products) {
+    return <div>Loading...</div>;
+  }
 
  
-  // return (
-  //   <div>
-  //     <h2>Product Page</h2>
-  //     <div className="product-container">
-  //     {/* <ProductCard key={product.id} product={product} /> */}
-  //       {products.map((product) => (
-  //         <ProductCard key={product.id} product={product} />
-  //       ))}
-  //     </div>
+  return (
+    <div>
+      <h2>Product Page</h2>
+      <div className="product-container">
+      {/* <ProductCard key={product.id} product={product} /> */}
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
       
-  //   </div>
- // );
+    </div>
+ );
 };
 
 export default ProductPage;
