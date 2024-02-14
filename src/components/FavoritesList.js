0f0113675b8f27d85e46d029614ac9c5382ac1af
lavliy <natarajanlavanya92@gmail.com>
2024-02-13 18:38:18 -0600

    editing FavoritesList

--- src/components/FavoritesList.js
+++ src/components/FavoritesList.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './FavoritesList.css';
+import {useNavigate} from 'react-router-dom'
+
 
 const FavoritesList = () => {
+
+  const navigate = useNavigate();
+
+  
+
   return (
-    <div className="favorites-list-container">
-      <div>
-        <h2 className="favorites-list-heading">Your Favorites List</h2>
-        {/*  favorites list content here */}
-      </div>
-      <footer className="footer">
-        {/* Footer content here */}
-      </footer>
+    <div>
+      <h2 className="favorites-list-heading">Your Favorites List</h2>
+      {/*  favorites list content here */}
     </div>
   );
 };
