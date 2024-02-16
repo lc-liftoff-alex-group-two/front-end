dc56f4fb69befd96d9ee72f83e128ab81aecdd01
lavliy <natarajanlavanya92@gmail.com>
2024-02-16 01:58:48 -0600

    adding fav list

--- src/components/FavoritesList.js
+++ src/components/FavoritesList.js
@@ -9,53 +9,84 @@ import React, { useState, useEffect } from "react";
 
 
 
-const FavoritesList = () => {
-  // const Auth = useAuth();
+const FavoritesList = ({userId}) => {
+  const Auth = useAuth();
   const [favorites, setFavorites] = useState([]);
-  // const navigate = useNavigate();
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
 
-  // useEffect(() => {
-  //   const fetchFavorites = async () => {
-  //     try {
-  //       if(!Auth){
-  //         navigate('/login')
-  //         return;
-  //       }
-  //       const authHeader = {
-  //         Authorization: giveWiseApi.basicAuth(Auth.getUser()),
-  //         "Content-Type": "application/json",
-  //       };
-  //       const response = await fetch(`http://localhost:8080/favorites/user/${Auth.getUser().id}`, {
-  //         headers: authHeader,
-  //       });
+  useEffect(({userId}) => {
+    const fetchFavorites = async () => {
+      try {
+        if(!Auth){
+          navigate('/login')
+          return;
+        }
+        const authHeader = {
+          Authorization: giveWiseApi.basicAuth(Auth.getUser()),
+          "Content-Type": "application/json",
+        };
+        const response = await fetch(`http://localhost:8080/favorites/${userId}`, {
+          headers: authHeader,
+        });
 
-  //       if (response.ok) {
-  //         const data = await response.json();
-  //         setFavorites(data);
-  //       } else {
-  //         console.error('Failed to fetch favorites');
-  //       }
-  //     } catch (error) {
-  //       console.error('Error fetching favorites:', error);
-  //     }
-  //   };
+        if (response.ok) {
+          const data = await response.json();
+          setFavorites(data);
+        } else {
+          console.error('Failed to fetch favorites');
+        }
+      } catch (error) {
+        console.error('Error fetching favorites:', error);
+      }
+    };
 
-  //   fetchFavorites();
-  // }, [Auth]); 
-  
+    fetchFavorites();
+  }, [Auth, userId, navigate]); 
+
 
   return (
     <div>
-      <h1>Favorites</h1>
+      <h2>Favorites</h2>
       <ul>
-        {favorites.map((favorite) => (
-          <li key={favorite.id}>{/* Render favorite details */}</li>
+      {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            
+          />
         ))}
       </ul>
-     
     </div>
   );
 };
 
 export default FavoritesList;
+
+  // useEffect(() => {
+  //   const fetchFavorites = async () => {
+  //     try {
+  //       // Make a request to the backend to fetch cart items with product details for the user ID
+  //       const response = await fetch(`http://localhost:8080/favorites/{userId}`);
+  //       const data = await response.json();
+  //       setFavorites(data);
+  //     } catch (error) {
+  //       console.error('Error fetching favorites items:', error);
+  //     }
+  //   };
+
+  //   fetchFavorites();
+  // }, [userId]);
+
+  // return (
+  //   <div>
+  //     <h1>Favorites</h1>
+  //     <ul>
+  //       {favorites.map((favorite) => (
+  //         <li key={favorite.id}>{/* Render favorite details */}</li>
+  //       ))}
+  //     </ul>
+     
+  //   </div>
+  // );
